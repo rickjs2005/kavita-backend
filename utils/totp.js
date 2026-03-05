3a5e21c650ae2ba0dbf8ca6c3772a5547ca87d45
rick js <113396445+rickjs2005@users.noreply.github.com>
2026-03-04 21:22:50 -0300

    Update utils/totp.js to use double quotes for ESLint compliance

--- utils/totp.js
+++ utils/totp.js
@@ -1,18 +1,7 @@
-const speakeasy = require('speakeasy');
+const speakeasy = require("speakeasy");
 
-/**
- * Verifies the TOTP token.
- *
- * @param {*} token - The TOTP token to verify.
- * @param {*} secret - The secret key used to generate the TOTP.
- * @returns {boolean} - Returns true if the token is valid, false otherwise.
- */
 function verifyToken(token, secret) {
-    return speakeasy.totp.verify({
-        secret: secret,
-        encoding: 'base32',
-        token: token
-    });
+    return speakeasy.totp.verify({ secret: secret, encoding: "base32", token: token });
 }
 
 module.exports = { verifyToken };
